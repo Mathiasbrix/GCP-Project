cdac76c77ef716bc3d0c639f74bd7f9428d89217
Mathias <mbrix@student.sdu.dk>
2022-12-28 21:10:29 +0100

    cleanup

--- Frontend/src/common/Constants.ts
+++ Frontend/src/common/Constants.ts
@@ -1,7 +1,7 @@
 import { Console } from "console";
 
 const constansts = {
-  serverBaseURL: "https://backend-7aeqgkv7xa-ey.a.run.app",
+  serverBaseURL: "localhost:3000",
 };
 
 export default constansts;
